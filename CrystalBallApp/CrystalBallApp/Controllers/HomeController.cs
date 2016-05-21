878e739fbf2670636a051e4a6c8e11f88974ce76
Melanie McGee <mel@wecancodeit.org>
2016-05-21 18:30:33 -0400

    Initial commit.

---
+++ CrystalBallApp/CrystalBallApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using CrystalBallApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalBallApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult ShakeBall()
+        {
+            RandomStrings rndStr = new RandomStrings();
+
+            return Json( rndStr.grabRandomString(), JsonRequestBehavior.AllowGet);
+        }
+
+     }
+}
---
+++ CrystalBallApp/CrystalBallApp/Models/RandomStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalBallApp.Models
+{
+   public class RandomStrings
+    {
+        private string[] availableStrings = { "Code is poetry","Ladies prefer coding", "We Can Code IT", "Puppies are cute"};
+        private Random rnd = new Random(); //just grabbing a random object so we can use it later
+
+        //get string
+        internal string grabRandomString()
+        {
+            return availableStrings[rnd.Next(0,availableStrings.Length)];
+        }
+    }
+}
